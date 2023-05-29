7384f754c2621d5b277d13e060f572adb508c8b8
HumbertoBPF <humbertobpf@gmail.com>
2023-05-29 13:49:45 -0300

    Database setup

--- django_basics/employee_registration/admin.py
+++ django_basics/employee_registration/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from employee_registration.models import Company
 
-# Register your models here.
+
+class CompanyDisplay(admin.ModelAdmin):
+    # Record columns to be displayed
+    list_display = ("name", "address", "branch")
+    # Record columns that, when clicked, redirect to the edit page
+    list_display_links = ("name",)
+    # Fields that are used in the search bar
+    search_fields = ("name", "address")
+    # Filter that appears on the right side of the page, grouping records by certain category
+    list_filter = ("branch",)
+    # Fields to determine the order in which the records are displayed
+    ordering = ("name",)
+    # Number of records per page
+    list_per_page = 10
+    # Renders a widget allowing to search for Users
+    autocomplete_fields = ("employees",)
+
+
+# Call this method to bind a model and a display configuration class
+admin.site.register(Company, CompanyDisplay)
--- django_basics/employee_registration/models.py
+++ django_basics/employee_registration/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Company(models.Model):
+    # First item: content to be stored in the actual table
+    # Second item: string to be rendered on the admin page
+    TYPE_CHOICES = [
+        ("IT", "IT"),
+        ("HEALTH", "Health"),
+        ("FINANCE", "Financial market"),
+        ("EDUCATION", "Education"),
+        ("ENERGY", "Energy"),
+        ("FOOD", "Food"),
+        ("OTHER", "Other")
+    ]
+
+    # CharField: used for small-medium texts (requires a "max_length" attr)
+    name = models.CharField(max_length=255)
+    # TextField: used for larger texts
+    address = models.TextField()
+    # DateTime: used for dates
+    foundation_date = models.DateField(default=timezone.now)
+    # "choices" attribute: used to only allow certain texts
+    branch = models.CharField(max_length=255, choices=TYPE_CHOICES)
+    # Relationship between User and Company table
+    # It's many from the company side, because a company can have many users as employees
+    # It's many from the user side, because a user can work for many companies
+    employees = models.ManyToManyField(User)
+
+    class Meta:
+        # String to be used when the plural version of the table name is needed
+        verbose_name_plural = "companies"
+
+    def __str__(self):
+        # String providing the textual representation of the table records
+        return self.name
